3da7424d7efac3e48acaefa19316ea2901318896
Kunal <62366085+KunalRajpal@users.noreply.github.com>
2021-06-07 11:34:42 -0500

    made border in draw

--- Snake game/main.cpp
+++ Snake game/main.cpp
@@ -23,7 +23,37 @@ void Setup()
 
 void Draw()
 {
+    system("cls"); //clears the system in terminal of windows
+    for(int i=0; i< width; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
 
+    for(int i=0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(j == 0)
+            {
+                cout << "#";
+            }
+            else{
+                cout << " ";
+            }
+
+            if(j == width -1)
+            {
+                cout << "#";
+            }
+        }
+        cout << endl;
+    }
+    for(int i=0; i <width+2; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
 }
 
 void Input()
