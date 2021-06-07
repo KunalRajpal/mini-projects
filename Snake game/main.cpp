5da2d58ac1a12aaf1479f0125c9aeb3ffd1b78da
Kunal <62366085+KunalRajpal@users.noreply.github.com>
2021-06-07 12:42:48 -0500

    Terminating bug fixed

--- Snake game/main.cpp
+++ Snake game/main.cpp
@@ -163,9 +163,15 @@ void Logic()
     }
 
     if(x > width || x <0 || y > height || y < 0)
-    {
         gameOver = true;
+    for(int i=0; i<nTail; i++)
+    {
+        if(tailX[i] && tailY[i] == y)
+        {
+            gameOver = true;
+        }
     }
+
 
     if(x == fruitX && y == fruitY)
     {
