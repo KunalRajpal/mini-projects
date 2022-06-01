dee0a29270b633fcb5104e3c64bbcb263267e4ef
Kunal <rajpal.kunal1@gmail.com>
2022-06-01 05:15:24 -0500

    added tasks

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState}  from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  //this is called object destructuring 
+   //? Read more here: https://www.youtube.com/watch?v=NIq3qLaHCIs
+   const [tasks, setTodos] = useState(['Task #1', 'Task #2'])
+
   return (
+    // tasks and todos here are called props
+    //basically like an argument
     <>
-    <TodoList />
+      <TodoList tasks = {tasks}/> 
+      <input type = "text"/>
+      <button>Add a task</button>
+      <button>Clear completed tasks</button>
+      <div># of tasks uncompleted</div>
     </>
   );
 }
---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TodoList( {tasks} ){
+
+    
+    return (
+        <div>
+            {tasks.length}
+        </div>
+    )
+}
